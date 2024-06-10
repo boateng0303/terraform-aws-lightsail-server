bb7008c012755c0b24306dd22421f45866f3e493
Kwasi <shev030391@gmail.com>
2024-06-09 22:43:38 -0400

    script_automation

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_lightsail_instance" "custom" {
   availability_zone = "us-east-1b"
   blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_3_0"
-  user_data         = "sudo yum install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>Deployed via Terraform</h1>' | sudo tee /var/www/html/index.html"
+  user_data         = file("script.sh")
   tags = {
     Team       = "DevOps"
     env        = "dev"
