2527502c3598f22fd1c719e8f30c1c6ef138b417
Kwasi <shev030391@gmail.com>
2024-06-09 21:48:21 -0400

    terraform codes

--- version.tf
+++ version.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.60.0"
     }
   }
@@ -9,5 +9,5 @@ terraform {
 
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
